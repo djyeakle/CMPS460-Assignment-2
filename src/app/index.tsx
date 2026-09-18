1b6a5bcdcabb386d401323a4f6ba8d414379b873
Delaney <djyeakl@pointpark.edu>
2026-09-18 16:54:40 -0400

    profile app complete

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,116 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+function TechSection() {
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
+    <View style={styles.card}>
+      <Text style={styles.sectionTitle}>Technical Skills</Text>
+
+      <Text style={styles.plainText}>
+        HTML, CSS, JavaScript, TypeScript, React, GitHub, PHP, C#, and
+        PostgreSQL
+      </Text>
+    </View>
   );
 }
 
-export default function HomeScreen() {
+function CareerGoals() {
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+    <View style={styles.card}>
+      <Text style={styles.sectionTitle}>Career Goals</Text>
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
+      <Text style={styles.plainText}>
+        I want to become a frontend web developer who creates educational video
+        games on the side. I want to be able to help people while also doing
+        what I love.
+      </Text>
+    </View>
+  );
+}
 
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Delaney Yeakle</Text>
 
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+      <View style={styles.mainCard}>
+        <Text style={styles.sectionTitle}>Profile Overview</Text>
+
+        <Text style={styles.plainText}>Age: 22</Text>
+
+        <Text style={styles.plainText}>Location: Millsboro, DE</Text>
+
+        <Text style={styles.plainText}>Email: djyeakl@pointpark.edu</Text>
+
+        <Text style={styles.plainText}>
+          Favorite Programming Languages: HTML and CSS
+        </Text>
+
+        <Text style={styles.plainText}>
+          Career Interest: Frontend Web Developer
+        </Text>
+      </View>
+
+      <View style={styles.card}>
+        <Text style={styles.sectionTitle}>Education</Text>
+
+        <Text style={styles.plainText}>School: Point Park University</Text>
+
+        <Text style={styles.plainText}>Major: Applied Computer Science</Text>
+
+        <Text style={styles.plainText}>Minor: Accounting</Text>
+
+        <Text style={styles.plainText}>Class: Senior</Text>
+      </View>
+
+      <TechSection />
+
+      <CareerGoals />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
+    padding: 30,
+    backgroundColor: "#aac5f2",
   },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
+
   title: {
-    textAlign: 'center',
+    fontSize: 40,
+    fontWeight: "bold",
+    marginBottom: 30,
   },
-  code: {
-    textTransform: 'uppercase',
+
+  card: {
+    padding: 20,
+    borderWidth: 2,
+    borderRadius: 10,
+    backgroundColor: "white",
+    marginBottom: 15,
   },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+
+  mainCard: {
+    padding: 20,
+    borderWidth: 3,
+    borderRadius: 10,
+    backgroundColor: "#cedcf2",
+    marginBottom: 15,
+  },
+
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  plainText: {
+    fontSize: 14,
+    fontWeight: "regular",
+    color: "black",
   },
 });
+
+export default App;
